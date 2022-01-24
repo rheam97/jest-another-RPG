dc6449eaadc4e06de1d763dc776532b90d7132c8
Rhea <rheam97@gmail.com>
2022-01-24 17:25:28 -0500

    added player prototype stats and inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,5 @@
 // const { jest, expect } = require('@jest/globals')
+const { expect } = require('@jest/globals')
 const Player = require('../lib/Player.js')
 const Potion= require('../lib/Potion')
 jest.mock=('../lib/Potion.js')
@@ -12,4 +13,21 @@ test('creates a Player object', () => {
     expect(player.strength).toEqual(expect.any(Number))
     expect(player.agility).toEqual(expect.any(Number))
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]))
+})
+
+test('get players stats as an object', ()=> {
+
+    const player = new Player('Rhea')
+    expect(player.getStats()).toHaveProperty('potions')
+    expect(player.getStats()).toHaveProperty('health')
+    expect(player.getStats()).toHaveProperty('strength')
+    expect(player.getStats()).toHaveProperty('agility')
+})
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Rhea')
+    expect(player.getInventory()).toEqual(expect.any(Array))
+
+    player.inventory=[]
+    expect(player.getInventory()).toEqual(false)
 })
